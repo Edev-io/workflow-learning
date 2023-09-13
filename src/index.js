5bc1a997a08929e54abbc1c1d8c6f73a201290b4
eurico77 <euricomagalhaes61@gmail.com>
2023-09-13 08:09:59 -0300

    add coverage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+const createUser = require('./services/users.service');
 const express = require('express');
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+app.post('/users', (req, res) => {
+  const { name, surName, age } = req.body;
+  const users = createUser({ name, surName, age });
+  return res.json(users).send();
 });
 
 app.listen(port, () => console.log(`Servidor está rodando na porta ${port}`));
